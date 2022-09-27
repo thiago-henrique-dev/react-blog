0412bffa0e75311fd8ea342cf2efc9d8a3fa88c6
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-27 14:40:05 -0300

    creating post and editing css

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Post.css'
+
+export default function Post() {
+  return (
+    <div className='post'>
+        <img className='postImage'
+             src="https://th.bing.com/th/id/OIP.E0J17-Lt3pwbL1SjtB1-mwHaE7?pid=ImgDet&rs=1" alt="" 
+             />
+             <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">Music</span>
+                    <span className="postCat">Life</span>
+                </div>
+                <span className="postTitle">Lorem ipsum dolor sit amet.</span>
+                <hr/>
+                <span className="postDate">1 hour ago</span>
+             </div>
+             <p className='postDesc'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+          tellus id condimentum lobortis. Cras nec convallis massa. Sed eleifend
+          sodales nisl ac bibendum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+          tellus id condimentum lobortis. Cras nec convallis massa. Sed eleifend
+          sodales nisl ac bibendum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+          tellus id condimentum lobortis. Cras nec convallis massa. Sed eleifend
+          sodales nisl ac bibendum</p>
+        </div>
+  )
+}
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Post from '../Post/Post'
 import './Posts.css'
 
 export default function Posts() {
   return (
-    <div className='posts'>Posts</div>
+    <div className='posts'>
+        <Post/>
+    </div>
   )
 }
