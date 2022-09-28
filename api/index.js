1e926775b21a56c2960104cb0d35d0dca378ddf1
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-28 16:43:43 -0300

    connected bd mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log("connected to db");
+    }).catch(err=> {
+        throw err;
+    })
+}
 
 app.listen("5000", () => {
+    connect()
     console.log("Backend is running")
 })
